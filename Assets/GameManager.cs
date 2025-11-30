523a818acf099ea16e2a08eb36496649e062a356
RemFdz <114572880+RemFdz@users.noreply.github.com>
2025-11-30 14:33:50 +0900

    feat: game over screen

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject titleScreenUI;
+    public GameObject endScreenUI;
+
+    private static GameManager instance;
+
+    private bool _restarting = false;
 
 
     private void Start()
     {
         PauseGame();
+
+        if (_restarting)
+        {
+            _restarting = false;
+            StartGame();
+        }
+        else
+        {
+            titleScreenUI.SetActive(true);
+            endScreenUI.SetActive(false);
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     private void PauseGame()
@@ -18,11 +47,27 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
     }
 
+    public void EndGame()
+    {
+        PauseGame();
+        endScreenUI.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        _restarting = true;
+        Time.timeScale = 1f;
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
     public void StartGame()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         titleScreenUI.SetActive(false);
+        endScreenUI.SetActive(false);
         Time.timeScale = 1f;
     }
 }
